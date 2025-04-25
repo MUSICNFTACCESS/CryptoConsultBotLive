84cb1d152aa09e6e72716371a88d0593d428b545
MUSICNFTACCESS <crimzncipriano@gmail.com>
2025-04-24 23:43:32 -0400

    fix: ensure index.html loads on all routes

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ const app = express();
 app.use(cors());
 app.use(express.static(__dirname));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log('CrimznBot is online'));
